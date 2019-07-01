1c73582f60f5f51bcad28f2af09dfcad95f4a3cd
Jeremy Yallop <yallop@gmail.com>
2019-07-01 15:17:23 +0100

    Expose typeable instances.

--- lib/syb_instances.mli
+++ lib/syb_instances.mli
@@ -9,4 +9,13 @@ implicit module Data_list {A: DATA} : DATA with type t = A.t list
 implicit module Data_pair {A: DATA} {B: DATA} : DATA with type t = A.t * B.t
 implicit module Data_option {A: DATA} : DATA with type t = A.t option
 
+module Typeable_int : TYPEABLE with type t = int
+module Typeable_bool : TYPEABLE with type t = bool
+module Typeable_float : TYPEABLE with type t = float
+module Typeable_string : TYPEABLE with type t = string
+module Typeable_list {A: TYPEABLE} : TYPEABLE with type t = A.t list
+module Typeable_pair {A: TYPEABLE} {B: TYPEABLE} : TYPEABLE with type t = A.t * B.t
+module Typeable_option {A: TYPEABLE} : TYPEABLE with type t = A.t option
+module Typeable_either {A: TYPEABLE} {B: TYPEABLE} : TYPEABLE with type t = (A.t, B.t) either
+
 implicit module Typeable_of_data {A: DATA} : TYPEABLE with type t = A.t
